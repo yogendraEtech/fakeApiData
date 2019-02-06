d00b537fb72362abd1515173eee2c59c108822ea
yogendra <you@example.com>
2019-02-06 19:29:03 +0530

    redux saga-getting images from api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { connect } from "react-redux";
 
 class App extends Component {
   render() {
+    const { fetching, dog, onRequestDog, error } = this.props;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={dog} className="App-logo" width='40%' height='400px'/>
+          <h1 className="App-title">Welcome to Dog Saga</h1>
         </header>
+
+        {dog ? (
+          <p className="App-intro">Keep clicking for new dogs</p>
+        ) : (
+          <p className="App-intro">click for watching dogs Gallery</p>
+        )}
+
+        {fetching ? (
+          <button className="btn btn-danger" disabled>&nbsp;&nbsp;&nbsp;Fetching.....&nbsp;&nbsp;&nbsp;</button>
+        ) : (
+          <button className="btn btn-success" onClick={onRequestDog}>Request a Dog</button>
+        )}
+
+        {error ? <p style={{ color: "red" }}>Uh oh - something went wrong!</p>:<p></p>}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    fetching: state.fetching,
+    dog: state.dog,
+    error: state.error
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onRequestDog: () => dispatch({ type: "API_CALL_REQUEST" })
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { Provider } from "react-redux";
+import { reducer } from "./redux";
+import { watcherSaga } from "./sagas";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// create a redux store with our reducer above and middleware
+let store = createStore(
+  reducer,
+  compose(applyMiddleware(sagaMiddleware))
+);
+
+// run the saga
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+serviceWorker.register();
